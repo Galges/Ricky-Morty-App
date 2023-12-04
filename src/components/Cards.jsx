d751868ea9a08a65ecb90a7cd896511be764f4fb
Galges <83497794+Galges@users.noreply.github.com>
2023-12-04 16:11:30 +0000

    spaces filter started to work

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,38 +1,76 @@
-import React, { useEffect, useState } from 'react'
-import './Cards.css'
+import React, { useEffect, useState } from 'react';
+import './Cards.css';
 
 function Cards({ data, space }) {
-	const [test, setTest] = useState([])
+  const [test, setTest] = useState([]);
+  const [fetchedData, setFetchedData] = useState([]);
 
-	useEffect(() => {
-		setTest(space)
-	})
+  useEffect(() => {
+    setTest(space);
+  }, [space]);
 
-	console.log(test)
-	return (
-		<>
-			<div className='boxes'>
-				{data.map(x => {
-					let { id, name, image, status, location } = x
+  useEffect(() => {
+    const fetchDataForLinks = async () => {
+      if (test.length > 0) {
+        const fetchedResults = await Promise.all(
+          test.map(async (link) => {
+            const response = await fetch(link);
+            const data = await response.json();
+            return data;
+          })
+        );
 
-					return (
-						<div key={id} className='box'>
-							<div className='box-Img'>
-								<img src={image} alt={name} />
-							</div>
-							<div className='status'>
-								<p className=''>{status}</p>
-							</div>
-							<div className='character-Info'>
-								<p className='name'>{name}</p>
-								<p className='location'>{location.name}</p>
-							</div>
-						</div>
-					)
-				})}
-			</div>
-		</>
-	)
+        setFetchedData(fetchedResults);
+      }
+    };
+
+    fetchDataForLinks();
+  }, [test]);
+
+  console.log(fetchedData);
+  return (
+    <>
+      <div className='boxes'>
+        {space.length === 0
+          ? data.map((x) => {
+              let { id, name, image, status, location } = x;
+
+              return (
+                <div key={id} className='box'>
+                  <div className='box-Img'>
+                    <img src={image} alt={name} />
+                  </div>
+                  <div className='status'>
+                    <p className=''>{status}</p>
+                  </div>
+                  <div className='character-Info'>
+                    <p className='name'>{name}</p>
+                    <p className='location'>{location.name}</p>
+                  </div>
+                </div>
+              );
+            })
+          : fetchedData.map((x) => {
+              let { id, name, image, status, location } = x;
+
+              return (
+                <div key={id} className='box'>
+                  <div className='box-Img'>
+                    <img src={image} alt={name} />
+                  </div>
+                  <div className='status'>
+                    <p className=''>{status}</p>
+                  </div>
+                  <div className='character-Info'>
+                    <p className='name'>{name}</p>
+                    <p className='location'>{location.name}</p>
+                  </div>
+                </div>
+              );
+            })}
+      </div>
+    </>
+  );
 }
 
-export default Cards
+export default Cards;
